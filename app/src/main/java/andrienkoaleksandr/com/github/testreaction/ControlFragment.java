7b465a88421ff08d182172d4c815fdb5b79a8fe4
Logarifm <logarifm6@gmail.com>
2014-10-20 23:52:08 +0300

    Deleted unused methods

--- app/src/main/java/andrienkoaleksandr/com/github/testreaction/ControlFragment.java
+++ app/src/main/java/andrienkoaleksandr/com/github/testreaction/ControlFragment.java
@@ -1,6 +1,5 @@
 package andrienkoaleksandr.com.github.testreaction;
 
-import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -10,36 +9,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.List;
-import java.util.concurrent.Callable;
-
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ControlFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ControlFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
 public class ControlFragment extends Fragment {
 
     private Button startButton;
 
     private TextView resultLabel;
 
-    private OnFragmentInteractionListener mListener;
-
     private ControlFragment fragment = this;
 
     private GameThread gameThread = new GameThread(fragment);
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     * @return A new instance of fragment ControlFragment.
-     */
     public static ControlFragment newInstance() {
         ControlFragment fragment = new ControlFragment();
         Bundle args = new Bundle();
@@ -58,45 +37,6 @@ public class ControlFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_control, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-//    @Override
-//    public void onAttach(Activity activity) {
-//        super.onAttach(activity);
-//        try {
-//            mListener = (OnFragmentInteractionListener) activity;
-//        } catch (ClassCastException e) {
-//            throw new ClassCastException(activity.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
     }
 
     @Override
@@ -124,13 +64,5 @@ public class ControlFragment extends Fragment {
 
     public TextView getResultLabel() {
         return resultLabel;
-    }
-
-    public void setStartButton(Button startButton) {
-        this.startButton = startButton;
-    }
-
-    public void setResultLabel(TextView resultLabel) {
-        this.resultLabel = resultLabel;
     }
 }
